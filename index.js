3d837f4c8c16dca5dcac581caaa9f406f9dcc3ae
Florian Romijn <florian.romijn@gmail.com>
2022-04-11 18:38:46 +0200

    env variable token configuration

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const {token} = require('./config.json')
+//const {token} = require('./config.json')
 const begroetingen = ["goedemorgen", "Goedemorgen", "goeiemoggel", "Goeiemoggel", "goededag", "Goededag", "mogguh", "Mogguh", "schöne daag", "Schöne daag"]
 
 client.on('ready', () => {
@@ -15,4 +15,5 @@ client.on('ready', () => {
     }
   });
 
-client.login(token)
+//client.login(token)
+client.login()
