d185e07376ed9c6435dafd73b47c353c59b6dc9c
Koen de Bont <koen.de.bont@maxserv.com>
2022-04-15 00:16:37 +0200

    moved dictionary to reactions.json and search in for loop

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const reactions = require('./reactions.json');
 //const {token} = require('./config.json')
-const begroetingen = ["goede ochtend", "Goede ochtend","goedemorgen", "Goedemorgen", "goeiemoggel", "Goeiemoggel", "goeiemorgen", "Goeiemorgen", "goededag", "Goededag", "mogguh", "Mogguh", "schöne daag", "Schöne daag", "morning", "Morning", "mogge", "Mogge", "goedemoggel", "Goedemoggel", "goede moggel", "Goede moggel"]
-const avondgroeten = ["goedenavond", "Goedenavond", "navond", "Navond", "goedenacht", "Goedenacht", "goede avond", "Goede avond", "weltrusten", "Weltrusten", "trusten", "Trusten", "gnite", "Gnite", "slaapze", "Slaapze", "fapze", "Fapze", "nightie", "Nightie", "slaaplekker", "Slaaplekker", "slaap lekker", "Slaap lekker", "goedenavund", "Goedenavund"]
-const weekend = ["het is weekend", "Het is weekend", "tis weekend", "Tis weekend", "weer weekend", "Weer weekend"]
+
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-  });
-  
-  client.on('messageCreate', async msg => {
-    if (msg.author.bot) return
-  
-    else if(begroetingen.some(word => msg.content.includes(word))){
-      msg.react('🌞')
-    }
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-    else if(avondgroeten.some(word => msg.content.includes(word))){
-      msg.react('✨')
-    }
-    
-    else if(weekend.some(word => msg.content.includes(word))){
-      msg.react('🎉')
-    }
+client.on('messageCreate', async msg => {
+  if (msg.author.bot) return
 
-  });
-  
+  for (const [emoji, words] of Object.entries(reactions)) {
+
+    if (search(words, msg.content))
+      msg.react(emoji)
+  }
+});
+
+function search(needles, haystack) {
+  return needles.some(needle => haystack.includes(needle.toLowerCase()))
+}
+
 //client.login(token)
 client.login(process.env.GMZZ_TOKEN)
